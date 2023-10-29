44633cb2b04ff699f19f75d03560d66453e99dc3
Wojciech Tartanus <wojciech.tartanus@synergycodes.com>
2023-10-29 14:00:06 +0100

    Removing ports on link deletion

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,21 @@ import './styles.css';
 window.addEventListener('load', () => {
     const diagramDiv = document.getElementById('diagram') as HTMLDivElement;
     const diagram = createDiagram(diagramDiv);
+
+    diagram.addDiagramListener('SelectionDeleted', function(e) {
+        if (e.subject.count === 1 && e.subject.first() instanceof go.Link) {
+            const link = e.subject.first() as go.Link;
+
+            diagram.startTransaction('removePorts');
+            const fromPortIndex = link.fromNode.data.ports
+                .findIndex((port: { portId: string }) => port.portId === link.fromPortId);
+            diagram.model.removeArrayItem(link.fromNode.data.ports, fromPortIndex);
+            
+            const toPortIndex = link.toNode.data.ports
+                .findIndex((port: { portId: string }) => port.portId === link.toPortId);
+            diagram.model.removeArrayItem(link.toNode.data.ports, toPortIndex);
+            
+            diagram.commitTransaction('removePorts');
+        }
+      });
 });
